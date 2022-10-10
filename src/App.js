51673b1806cb77a9d555cbeb741f98bca8c49be3
Lucas Victor <lucassvtf@gmail.com>
2022-10-10 16:11:06 -0300

    Requisito 16 - começo do requisito

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Ranking from './pages/Ranking';
+import Feedback from './pages/Feedback';
 import './App.css';
 
 // import logo from './trivia.png';
@@ -13,6 +15,8 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/game" component={ Game } />
       <Route path="/settings" component={ Settings } />
+      <Route path="/ranking" component={ Ranking } />
+      <Route path="/feedback" component={ Feedback } />
     </Switch>
     // <div className="App">
     //   <header className="App-header">
