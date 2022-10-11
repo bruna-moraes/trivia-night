4569173fd9a1b61cf5c7b670337fca192d93e493
Bruna Moraes <brunasthephany.moraes@gmail.com>
2022-10-11 11:37:23 -0300

    feat: Adiciona botão para voltar ao início

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Header from '../components/Header';
 
 class Ranking extends React.Component {
+  goToHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <h1 data-testid="ranking-title">Ranking</h1>
+        <div>
+          <h1 data-testid="ranking-title">Ranking</h1>
+          <button
+            type="button"
+            data-testid="btn-go-home"
+            onClick={ this.goToHome }
+          >
+            Início
+          </button>
+        </div>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.array,
+}.isRequired;
 
 export default Ranking;
